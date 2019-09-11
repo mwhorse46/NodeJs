65410f474dec4fbb5fdbd25dcc9922a1fc3378b8
mwhorse46 <needmefordev@gmail.com>
2019-09-11 15:46:36 +0530

    connection and import library

--- connection_import-library.js
+++ connection_import-library.js
@@ -1,6 +1,6 @@
-var http=require('http');  //
+var http=require('http');  //import library
 http.createServer(function(req,res){
     res.end("Hello Node\n");
-}).listen(3000);
+}).listen(3000);  //port no. 3000
 
 console.log("server is running on http://127.0.0.1:3000/")
