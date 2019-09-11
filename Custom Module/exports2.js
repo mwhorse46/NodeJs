e68a89a1694fd964ed3c36ade4ce998882875baf
mwhorse46 <needmefordev@gmail.com>
2019-09-11 21:48:40 +0530

    2nd method of export

---
+++ Custom Module/exports2.js
@@ -0,0 +1,23 @@
+// module.exports={
+
+//     sayHelloInEnglish : function(){
+//         return 'Hello';
+//     },
+    
+//     sayHelloInSpanish : function(){
+//         return 'Hola';
+//     }
+// };
+
+var x={
+
+    sayHelloInEnglish : function(){
+        return 'Hello';
+    },
+    
+    sayHelloInSpanish : function(){
+        return 'Hola';
+    }
+};
+
+module.exports=x;
